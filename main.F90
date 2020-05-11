031b9de8a3d06a9e45492211801b42dbd116acdc
Sam Hatfield <samuel.hatfield@ecmwf.int>
2020-05-11 17:13:27 +0100

    Make neural network precision-agnostic

--- main.F90
+++ main.F90
@@ -1,11 +1,11 @@
 PROGRAM MAIN
     USE NOGWDNN_MOD, ONLY: NN, NN_TL
     USE YONOGWDNN
-    USE PARKIND1, ONLY: JPRM
+    USE PARKIND1, ONLY: JPRB
 
     IMPLICIT NONE
     
-    REAL(JPRM) :: X1(NINP), X2(NINP), Y1(NOUT), Y2(NOUT), Y3(NOUT), DX(NINP), DY(NOUT), DIFF
+    REAL(JPRB) :: X1(NINP), X2(NINP), Y1(NOUT), Y2(NOUT), Y3(NOUT), DX(NINP), DY(NOUT), DIFF
     INTEGER :: I
     
     ALLOCATE(INPUT_B(NWIDTH))
@@ -26,7 +26,7 @@ PROGRAM MAIN
     
     DO I = 0, 7
         CALL INITIALIZE1(DX)
-        DX = DX*10.0_JPRM**(-REAL(I, JPRM))
+        DX = DX*10.0_JPRB**(-REAL(I, JPRB))
         X2 = X1 + DX
 
         WRITE (*,'(A)') "====================================================="
@@ -47,51 +47,51 @@ PROGRAM MAIN
     
 CONTAINS
     SUBROUTINE INITIALIZE1(X)
-        REAL(JPRM), INTENT(OUT) :: X(:)
+        REAL(JPRB), INTENT(OUT) :: X(:)
         
         INTEGER :: I
         
         DO I = 1, SIZE(X)
-            X(I) = RANDN(0.0_JPRM, 1.0_JPRM)
+            X(I) = RANDN(0.0_JPRB, 1.0_JPRB)
         END DO
     END SUBROUTINE INITIALIZE1
 
     SUBROUTINE INITIALIZE2(X)
-        REAL(JPRM), INTENT(OUT) :: X(:,:)
+        REAL(JPRB), INTENT(OUT) :: X(:,:)
         
         INTEGER :: I, J
         
         DO I = 1, SIZE(X, 1)
             DO J = 1, SIZE(X, 2)
-                X(I, J) = RANDN(0.0_JPRM, 1.0_JPRM)
+                X(I, J) = RANDN(0.0_JPRB, 1.0_JPRB)
             END DO
         END DO
     END SUBROUTINE INITIALIZE2
 
     SUBROUTINE INITIALIZE3(X)
-        REAL(JPRM), INTENT(OUT) :: X(:,:,:)
+        REAL(JPRB), INTENT(OUT) :: X(:,:,:)
         
         INTEGER :: I, J, K
         
         DO I = 1, SIZE(X, 1)
             DO J = 1, SIZE(X, 2)
                 DO K = 1, SIZE(X, 3)
-                    X(I, J, K) = RANDN(0.0_JPRM, 1.0_JPRM)
+                    X(I, J, K) = RANDN(0.0_JPRB, 1.0_JPRB)
                 END DO
             END DO
         END DO
     END SUBROUTINE INITIALIZE3
 
     FUNCTION RANDN(MEAN, STDEV)
-        REAL(JPRM), INTENT(IN) :: MEAN, STDEV
-        REAL(JPRM) :: U, V, RANDN
-        REAL(JPRM) :: RAND(2)
+        REAL(JPRB), INTENT(IN) :: MEAN, STDEV
+        REAL(JPRB) :: U, V, RANDN
+        REAL(JPRB) :: RAND(2)
 
         CALL RANDOM_NUMBER(RAND)
 
         ! Box-Muller method
-        U = (-2.0_JPRM * LOG(RAND(1))) ** 0.5_JPRM
-        V =   2.0_JPRM * 6.28318530718_JPRM * RAND(2)
+        U = (-2.0_JPRB * LOG(RAND(1))) ** 0.5_JPRB
+        V =   2.0_JPRB * 6.28318530718_JPRB * RAND(2)
         RANDN = MEAN + STDEV * U * SIN(V)
     END FUNCTION RANDN
 END PROGRAM MAIN
--- nogwdnn_mod.F90
+++ nogwdnn_mod.F90
@@ -1,16 +1,46 @@
 MODULE NOGWDNN_MOD
 
-USE PARKIND1, ONLY: JPRM
+USE PARKIND1
 USE YONOGWDNN
 
 IMPLICIT NONE
+
+PUBLIC :: GEMV
+INTERFACE GEMV
+   SUBROUTINE DGEMV(TRANS, M, N, ALPHA, A, LDA, X, INCX, BETA, Y, INCY)
+       USE PARKIND1, ONLY: JPRD
+       CHARACTER, INTENT(IN) :: TRANS
+       INTEGER, INTENT(IN) :: M, N
+       REAL(KIND=JPRD), INTENT(IN) :: ALPHA
+       REAL(KIND=JPRD), INTENT(IN), DIMENSION(LDA,*) :: A
+       INTEGER, INTENT(IN) :: LDA
+       REAL(KIND=JPRD), INTENT(IN), DIMENSION(*) :: X
+       INTEGER, INTENT(IN) :: INCX
+       REAL(KIND=JPRD), INTENT(IN) :: BETA
+       REAL(KIND=JPRD), INTENT(INOUT), DIMENSION(*) :: Y
+       INTEGER, INTENT(IN) :: INCY
+   END SUBROUTINE DGEMV
+   SUBROUTINE SGEMV(TRANS, M, N, ALPHA, A, LDA, X, INCX, BETA, Y, INCY)
+       USE PARKIND1, ONLY: JPRM
+       CHARACTER, INTENT(IN) :: TRANS
+       INTEGER, INTENT(IN) :: M, N
+       REAL(KIND=JPRM), INTENT(IN) :: ALPHA
+       REAL(KIND=JPRM), INTENT(IN), DIMENSION(LDA,*) :: A
+       INTEGER, INTENT(IN) :: LDA
+       REAL(KIND=JPRM), INTENT(IN), DIMENSION(*) :: X
+       INTEGER, INTENT(IN) :: INCX
+       REAL(KIND=JPRM), INTENT(IN) :: BETA
+       REAL(KIND=JPRM), INTENT(INOUT), DIMENSION(*) :: Y
+       INTEGER, INTENT(IN) :: INCY
+   END SUBROUTINE SGEMV
+END INTERFACE GEMV
 
 CONTAINS
 
 ! ---------------------------------------------------------------------------------
 
-ELEMENTAL REAL(KIND=JPRM) FUNCTION NONLINEARITY(X)
-  REAL(KIND=JPRM), INTENT(IN) :: X
+ELEMENTAL REAL(KIND=JPRB) FUNCTION NONLINEARITY(X)
+  REAL(KIND=JPRB), INTENT(IN) :: X
 
   IF (LTANH) THEN
         NONLINEARITY = TANH(X)
@@ -20,11 +50,11 @@ END FUNCTION NONLINEARITY
 
 ! ---------------------------------------------------------------------------------
 
-ELEMENTAL REAL(KIND=JPRM) FUNCTION NONLINEARITY_TL(X)
-    REAL(KIND=JPRM), INTENT(IN) :: X
+ELEMENTAL REAL(KIND=JPRB) FUNCTION NONLINEARITY_TL(X)
+    REAL(KIND=JPRB), INTENT(IN) :: X
 
     IF (LTANH) THEN
-        NONLINEARITY_TL = 1.0_JPRM - TANH(X)**2.0_JPRM
+        NONLINEARITY_TL = 1.0_JPRB - TANH(X)**2.0_JPRB
         RETURN
     END IF
 END FUNCTION NONLINEARITY_TL
@@ -33,30 +63,32 @@ END FUNCTION NONLINEARITY_TL
 
 SUBROUTINE NN(X,Y)
 
-  REAL(KIND=JPRM), INTENT(IN) :: X(:)
-  REAL(KIND=JPRM), INTENT(OUT) :: Y(:)
-  REAL(KIND=JPRM), ALLOCATABLE :: T1(:),T2(:)
+  REAL(KIND=JPRB), INTENT(IN) :: X(:)
+  REAL(KIND=JPRB), INTENT(OUT) :: Y(:)
+  REAL(KIND=JPRB), ALLOCATABLE :: T1(:),T2(:)
 
   INTEGER :: I
   
   ALLOCATE(T1(NWIDTH),T2(NWIDTH))
-  
+
+
   !FIRST LAYER
   T1 = INPUT_B
   !IF (ERRORCHECK(T1)) PRINT*,0,T1
-  CALL DGEMV('N',NWIDTH,NINP,1.0_JPRM,INPUT,NWIDTH,&
-       & X,1,1._JPRM,T1,1)
+  CALL GEMV('N',NWIDTH,NINP,1.0_JPRB,INPUT,NWIDTH,&
+       & X,1,1._JPRB,T1,1)
   !IF (ERRORCHECK(T1)) PRINT*,0.5,T1
   T2 = NONLINEARITY(T1)
   !IF (ERRORCHECK(T2)) PRINT*,1,T2
   !print*,T2
 
+
   !More hidden layers
   DO I = 1,NHIDDEN-1
      T1 = HIDDEN_B(:,I)
      !IF (ERRORCHECK(T1)) PRINT*,I+0.25,T1
-     CALL DGEMV('N',NWIDTH,NWIDTH,1.0_JPRM,HIDDEN(:,:,I),NWIDTH,&
-          & T2,1,1._JPRM,T1,1)
+     CALL GEMV('N',NWIDTH,NWIDTH,1.0_JPRB,HIDDEN(:,:,I),NWIDTH,&
+          & T2,1,1._JPRB,T1,1)
      !IF (ERRORCHECK(T1)) PRINT*,I+0.5,T1
      T2 = NONLINEARITY(T1)     
      !IF (ERRORCHECK(T2)) PRINT*,I+1,T2
@@ -65,8 +97,8 @@ SUBROUTINE NN(X,Y)
 
   Y = OUTPUT_B
   !IF (ERRORCHECK(Y)) PRINT*,NHIDDEN+0.25,Y
-  CALL DGEMV('N',NOUT,NWIDTH,1.0_JPRM,OUTPUT,NOUT,&
-       & T2,1,1._JPRM,Y,1)
+  CALL GEMV('N',NOUT,NWIDTH,1.0_JPRB,OUTPUT,NOUT,&
+       & T2,1,1._JPRB,Y,1)
   !IF (ERRORCHECK(Y)) PRINT*,NHIDDEN+0.5,Y
   !print*,Y
 
@@ -75,31 +107,31 @@ END SUBROUTINE NN
 ! ---------------------------------------------------------------------------------
 
 SUBROUTINE NN_TL(X, DX, Y, DY)
-    REAL(KIND=JPRM), INTENT(IN) :: X(:), DX(:)
-    REAL(KIND=JPRM), INTENT(OUT) :: Y(:), DY(:)
+    REAL(KIND=JPRB), INTENT(IN) :: X(:), DX(:)
+    REAL(KIND=JPRB), INTENT(OUT) :: Y(:), DY(:)
 
-    REAL(KIND=JPRM), ALLOCATABLE :: T1(:), T2(:), T_NL(:)
+    REAL(KIND=JPRB), ALLOCATABLE :: T1(:), T2(:), T_NL(:)
     INTEGER :: I
 
     ALLOCATE(T1(NWIDTH), T2(NWIDTH), T_NL(NWIDTH))
 
     ! Weight matrix times input perturbation
-    CALL DGEMV('N', &
+    CALL GEMV('N', &
         & NWIDTH, NINP, &
-        & 1.0_JPRM, &
+        & 1.0_JPRB, &
         & INPUT, NWIDTH, &
         & DX, 1, &
-        & 0.0_JPRM, &
+        & 0.0_JPRB, &
         & T1, 1)
 
     ! Nonlinear "trajectory"
     T_NL = INPUT_B
-    CALL DGEMV('N', &
+    CALL GEMV('N', &
         & NWIDTH, NINP, &
-        & 1.0_JPRM, &
+        & 1.0_JPRB, &
         & INPUT, NWIDTH, &
         & X, 1, &
-        & 1.0_JPRM, &
+        & 1.0_JPRB, &
         & T_NL, 1)
 
     T2 = NONLINEARITY_TL(T_NL) * T1
@@ -107,121 +139,121 @@ SUBROUTINE NN_TL(X, DX, Y, DY)
     ! Hidden layers
     DO I = 1, NHIDDEN - 1
         ! Weight matrix times input perturbation
-        CALL DGEMV('N', &
+        CALL GEMV('N', &
             & NWIDTH, NWIDTH, &
-            & 1.0_JPRM, &
+            & 1.0_JPRB, &
             & HIDDEN(:,:,I), NWIDTH, &
             & T2, 1, &
-            & 0.0_JPRM, &
+            & 0.0_JPRB, &
             & T1, 1)
 
         ! Nonlinear "trajectory"
         T2 = NONLINEARITY(T_NL)
         T_NL = HIDDEN_B(:,I)
-        CALL DGEMV('N', &
+        CALL GEMV('N', &
             & NWIDTH, NWIDTH, &
-            & 1.0_JPRM, &
+            & 1.0_JPRB, &
             & HIDDEN(:,:,I), NWIDTH, &
             & T2, 1, &
-            & 1.0_JPRM, &
+            & 1.0_JPRB, &
             & T_NL, 1)
 
         T2 = NONLINEARITY_TL(T_NL) * T1
     END DO
 
     ! Output layer (no activation function)
-    CALL DGEMV('N', &
+    CALL GEMV('N', &
         & NOUT, NWIDTH, &
-        & 1.0_JPRM, &
+        & 1.0_JPRB, &
         & OUTPUT, NOUT, &
         & T2, 1, &
-        & 0.0_JPRM, &
+        & 0.0_JPRB, &
         & DY, 1)
 
     ! Nonlinear "trajectory"
     T2 = NONLINEARITY(T_NL)
     Y = OUTPUT_B
     T_NL = HIDDEN_B(:,I)
-    CALL DGEMV('N', &
+    CALL GEMV('N', &
         & NOUT, NWIDTH, &
-        & 1.0_JPRM, &
+        & 1.0_JPRB, &
         & OUTPUT, NOUT, &
         & T2, 1, &
-        & 1.0_JPRM, &
+        & 1.0_JPRB, &
         & Y, 1)
 END SUBROUTINE NN_TL
 
 ! ---------------------------------------------------------------------------------
 
 SUBROUTINE NN_AD(X, GRADY, GRADX)
-    REAL(KIND=JPRM), INTENT(IN) :: X(:), GRADY(:)
-    REAL(KIND=JPRM), INTENT(OUT) :: GRADX(:)
+    REAL(KIND=JPRB), INTENT(IN) :: X(:), GRADY(:)
+    REAL(KIND=JPRB), INTENT(OUT) :: GRADX(:)
 
-    REAL(KIND=JPRM), ALLOCATABLE :: T1(:), T2(:), T_NL(:)
+    REAL(KIND=JPRB), ALLOCATABLE :: T1(:), T2(:), T_NL(:)
     INTEGER :: I, J
 
     ALLOCATE(T1(NWIDTH), T2(NWIDTH), T_NL(NWIDTH))
 
     ! Output layer (no activation function)
-    CALL DGEMV('T', &
+    CALL GEMV('T', &
         & NWIDTH, NOUT, &
-        & 1.0_JPRM, &
+        & 1.0_JPRB, &
         & OUTPUT, NOUT, &
         & GRADY, 1, &
-        & 0.0_JPRM, &
+        & 0.0_JPRB, &
         & T1, 1)
 
     ! Hidden layers
     DO I = NHIDDEN-1, 1, -1
         ! Compute nonlinear trajectory up to this hidden layer
         T_NL = INPUT_B
-        CALL DGEMV('N', &
+        CALL GEMV('N', &
             & NWIDTH, NINP, &
-            & 1.0_JPRM, &
+            & 1.0_JPRB, &
             & INPUT, NWIDTH, &
             & X, 1, &
-            & 1.0_JPRM, &
+            & 1.0_JPRB, &
             & T_NL, 1)
 
         DO J = 1, I
            T2 = NONLINEARITY(T_NL)
            T_NL = HIDDEN_B(:,J)
-           CALL DGEMV('N', &
+           CALL GEMV('N', &
                & NWIDTH, NWIDTH, &
-               & 1.0_JPRM, &
+               & 1.0_JPRB, &
                & HIDDEN(:,:,J), NWIDTH, &
                & T2, 1, &
-               & 1.0_JPRM, &
+               & 1.0_JPRB, &
                & T_NL, 1)
         END DO
 
         T2 = NONLINEARITY_TL(T_NL) * T1
-        CALL DGEMV('T', &
+        CALL GEMV('T', &
             & NWIDTH, NWIDTH, &
-            & 1.0_JPRM, &
+            & 1.0_JPRB, &
             & HIDDEN(:,:,I), NWIDTH, &
             & T2, 1, &
-            & 0.0_JPRM, &
+            & 0.0_JPRB, &
             & T1, 1)
     END DO
 
     T_NL = INPUT_B
-    CALL DGEMV('N', &
+    CALL GEMV('N', &
         & NWIDTH, NINP, &
-        & 1.0_JPRM, &
+        & 1.0_JPRB, &
         & INPUT, NWIDTH, &
         & X, 1, &
-        & 1.0_JPRM, &
+        & 1.0_JPRB, &
         & T_NL, 1)
 
     ! Input layer
     T2 = NONLINEARITY_TL(T_NL) * T1
-    CALL DGEMV('T', &
+    CALL GEMV('T', &
         & NINP, NWIDTH, &
-        & 1.0_JPRM, &
+        & 1.0_JPRB, &
         & INPUT, NWIDTH, &
         & T2, 1, &
-        & 0.0_JPRM, &
+        & 0.0_JPRB, &
         & GRADX, 1)
 END SUBROUTINE NN_AD
 
